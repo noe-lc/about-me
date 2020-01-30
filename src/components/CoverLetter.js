9878c32c42db2cd9f0588a0fb0a742ba3d1e5078
Noe <nl@datalabmx.com>
2020-01-29 22:26:47 -0600

    first commit

---
+++ src/components/CoverLetter.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './CoverLetter.css';
+
+export default () => {
+  return(
+    <div className='cover-letter'>
+      <div className='img-container'>
+        <img />
+      </div>
+      <div className="desc-container">
+        <h1>Hello!</h1>
+        <p>
+          My name is Noé Landaverde and I am a Mexican Geoinformatics Engineer.
+          Although my profession is heavily oriented towards Geographic Information
+          Systems and Cartography, my job experience and postgraduate education were
+          focused more on the technological/visual aspect of its core: analysing & processing
+          geographic information and developing web applications for visualization and
+          cartographic purposes.
+        </p>
+        <p>
+          I consider myself open-minded, versatile and adaptable very adaptable, as well
+          as always looking forward to learning more and improving in all aspects possible.
+        </p>
+      </div>
+    </div>
+  )
+}
+
