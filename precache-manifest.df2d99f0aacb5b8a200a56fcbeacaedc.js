07d28d6a0656f6d7e455f3c0f74feecd107d4250
Noe <nl@datalabmx.com>
2020-02-01 17:57:18 -0600

    Updates

--- precache-manifest.df2d99f0aacb5b8a200a56fcbeacaedc.js
+++ precache-manifest.df2d99f0aacb5b8a200a56fcbeacaedc.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "b9a3f91dfd48b9a8f9808cb115af238a",
+    "revision": "ad945b004c438d86aa175c71caa7d5ee",
     "url": "/about-me/index.html"
   },
   {
