ac6f94714bb612afebd214dbe8d53e399cf9f9e6
Noe <nl@datalabmx.com>
2020-02-01 17:05:36 -0600

    Updates

--- precache-manifest.d44ce8dc274a52a4df33f2ea7a8fe994.js
+++ precache-manifest.d44ce8dc274a52a4df33f2ea7a8fe994.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d64b7c82f0aa7300d0ebded5815f5cd7",
+    "revision": "a4b332c731e143d31c23069884caaaae",
     "url": "/about-me/index.html"
   },
   {
-    "revision": "fcb4479163aeb0b3481d",
-    "url": "/about-me/static/css/main.d43feb5d.chunk.css"
+    "revision": "93f9a0462026eae23a25",
+    "url": "/about-me/static/css/main.2eb16804.chunk.css"
   },
   {
     "revision": "39ee319d13d34e645c5f",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/about-me/static/js/2.4c0e2c29.chunk.js.LICENSE"
   },
   {
-    "revision": "fcb4479163aeb0b3481d",
-    "url": "/about-me/static/js/main.721c7468.chunk.js"
+    "revision": "93f9a0462026eae23a25",
+    "url": "/about-me/static/js/main.b989b69b.chunk.js"
   },
   {
     "revision": "2ddde02664d260693d2c",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/about-me/precache-manifest.a4c322d2feeafbae600ad7d813651db9.js"
+  "/about-me/precache-manifest.d44ce8dc274a52a4df33f2ea7a8fe994.js"
 );
 
 self.addEventListener('message', (event) => {
