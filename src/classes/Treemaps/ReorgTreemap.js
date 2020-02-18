d5f446f31eb88b476459165dc4fcbe4067c44139
Noe <nl@datalabmx.com>
2020-02-17 23:13:29 -0600

    minimum displacement & drag tolerance

--- src/classes/Treemaps/ReorgTreemap.js
+++ src/classes/Treemaps/ReorgTreemap.js
@@ -27,62 +27,77 @@ export class ReorgTreemap extends Treemap {
   }
 
   enableDrag = (sel) => {
-    let evt, rest, isLast = false;
-    const self = this;
-
-    const reset = (sel) => { 
-      sel.attr('style',null)
-        .transition()
-        .duration(250)
-        .style('position','relative')
+    let subject, clone, rest, wTransition = false;
+    let initL, initR;
+    const reset = (subject,wTransition) => { 
+      subject.attr('style',null)
+        .style('position','relative');
+      if(wTransition) {
+        clone.transition()
+          .duration(750)
+          .style('padding',0)
+          .style('width','0px')
+          .remove();
+        return;
+      }
+      clone.remove();
     };
     const reInsert = (refNode,node,before = true) => {
-      console.log('node.nextElementSibling :', node.nextElementSibling);
       refNode.parentElement.insertBefore(refNode,before ? node : node.nextElementSibling);
     };
 
     sel.call(
       d3.drag()
         .subject(d => sel.filter(dt => dt === d))
-        .on('start',function(d,_,nodes) { 
+        .on('start',function(d) {
+          subject = d3.event.subject;
           rest = sel.filter(dt => dt !== d);
-          isLast = this.isSameNode(nodes[nodes.length -1]);
+          ({ left: initL, right: initR } = this.getBoundingClientRect());
+          initL += 10; // tolerance 
+          initR += 10;
+          clone = subject.clone(true)
+            .classed('clone',true)
+            .datum(null);
          })
         .on('drag',function() { // TODO: set a "snapped" status to animate elements?
-          evt = d3.event;
-          evt.subject
+          subject
             .style('position','absolute')
-            .style('left',evt.x + 'px')
-            //.style('top',evt.y + 'px');
+            .style('left',d3.event.x - (parseInt(subject.style('width')) / 2) + 'px');
         })
         .on('end',function() {
-          const evtNode = evt.subject.node();
-          const nodes = rest.nodes();
-          const lastRect = nodes[nodes.length - 1].getBoundingClientRect();
-          const { left } = evtNode.getBoundingClientRect();
-          
-          if(isLast && left > lastRect.left) { // no change from last position
-            evt.subject.call(reset);
-          } else if(!isLast && left > lastRect.right) { // moved to last position
-            evtNode.parentElement.appendChild(evtNode);
-            evt.subject.call(reset);
-          } else { // in between and all other previous
-            for(let n of nodes) {
-              let rect = n.getBoundingClientRect()
-              if(left < rect.left) {
-                reInsert(evtNode,n);
-                evt.subject.call(reset);
-                break;
-              } else if(left > rect.right) {
-                reInsert(evtNode,n,false);
-                evt.subject.call(reset);
-                break;
+          const nodes = rest.nodes(),
+            lastRect = nodes[nodes.length - 1].getBoundingClientRect(),
+            isLast = this.isSameNode(nodes[nodes.length -1]),
+            { left, right } = this.getBoundingClientRect();
+
+          const compare = () => {
+            if(!isLast && left > lastRect.right) { // moved to last position
+              this.parentElement.appendChild(this);
+              wTransition = true;
+            } else if(!(isLast && left > lastRect.left)) { // in between and all other previous
+              for(let n of nodes) {
+                let rect = n.getBoundingClientRect();
+                if(left < rect.left) {
+                  reInsert(this,n);
+                  wTransition = true;
+                  break;
+                } else if(left > rect.right) {
+                  reInsert(this,n,false);
+                  wTransition = true;
+                  break;
+                }
               }
             }
-          }
-          sel = d3.select(evtNode.parentElement).selectAll('.nest-option'); // reassign selection after reordering
-          isLast = false;
+          };
           
+          if(left > initR || right < initL) { // moved beyond previous
+            compare();
+          }
+
+          reset(d3.event.subject,wTransition);
+          sel = d3.select(this.parentElement).selectAll('.nest-option') // reassign selection after reordering
+            .filter(d => d);
+          wTransition = false;
         })
     )
   }
